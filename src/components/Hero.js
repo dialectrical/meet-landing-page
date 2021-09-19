41c78b1be6c499454844b7d559f674fa4cc10e0f
Michael Camele <michael.camele@gmail.com>
2021-09-19 15:29:39 -0700

    first commit

---
+++ src/components/Hero.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export const Hero = () => {
+  return (
+    <section>
+      <img />
+      <div>
+      <h2>Group Chat for Everyone</h2>
+      <p>subtitle</p>
+      <button>button 1</button>
+      <button>button 2</button>
+      </div>
+      <img />
+    </section>
+  )
+}
---
+++ src/components/styles/StyledHeroContainer.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const StyledHeroContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  height: 358px;
+  width: 100%;
+  h2 {
+    margin: 0;
+    font-size: 64px;
+    line-height: 64px;
+  }
+`
