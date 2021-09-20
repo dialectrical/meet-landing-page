3bd19da2025fc4b478ba5abade856a6cbdbed3c6
dialectrical <michael.camele@gmail.com>
2021-09-19 22:25:33 -0700

    feat: add footer content

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,26 @@
 import React from "react";
 import { StyledFooterContainer } from "./styles/StyledFooterContainer";
+import { StyledButton } from "./styles/StyledButton";
 import imageFooter from "../img/image-footer.jpg";
+import { Theme } from "./Theme";
 
 export const Footer = () => {
+  const primaryButton = Theme;
   return (
     <StyledFooterContainer>
       <img src={imageFooter} />
       <div />
+      <h3>Experience more together</h3>
+      <p>
+        Stay connected with reliable HD meetings and unlimited one-on-one and
+        group video sessions.
+      </p>
+      <StyledButton
+        theme={primaryButton}
+        style={{ backgroundColor: "#D9B8FF" }}
+      >
+        Download <span>v1.3</span>
+      </StyledButton>
     </StyledFooterContainer>
   );
 };
