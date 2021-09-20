feaa33bc732e3aa629d4504a9dedbdf118da7e78
dialectrical <>
2021-09-19 19:53:03 -0700

    feat: add header and logo

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import { Hero } from "./components/Hero";
+import { Header } from "./components/Header";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Hero />
     </div>
   );
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import React from "react";
+import logo from "../img/logo.svg";
 
 export const Header = () => {
-  
-}
+  return <img src={logo} style={{ margin: "80px 660px 108px 661px" }} />;
+};
