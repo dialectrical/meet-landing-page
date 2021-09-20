5863c29435a498e7263cf3fda5b1384ae8018933
dialectrical <>
2021-09-19 18:43:58 -0700

    feat: LandingPage scaffold

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -2,5 +2,12 @@ import React from "react";
 import styled from "styled-components";
 
 export const LandingPage = () => {
-
-}
+  return (
+    <>
+      <Header />
+      <Hero />
+      <Content />
+      <Footer />
+    </>
+  );
+};
--- src/components/styles/Theme.js
+++ src/components/styles/Theme.js
@@ -1,10 +1,8 @@
 export const Theme = {
-  primaryButton:
-    {background: "#4D96A9",
-    text: "#8FE3F9",
-    hover: "#71C0D4"},
+  primaryButton: { background: "#4D96A9", text: "#8FE3F9", hover: "#71C0D4" },
   secondaryButton: {
-  background: "#855FB1",
-  text: "#D9B8FF",
-  hover: "#B18BDD"}
-}
+    background: "#855FB1",
+    text: "#D9B8FF",
+    hover: "#B18BDD",
+  },
+};
