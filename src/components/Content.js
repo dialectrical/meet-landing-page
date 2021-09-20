43cb7b73d5ea7c73fa39b4067f72cadd1cf91469
dialectrical <michael.camele@gmail.com>
2021-09-19 20:04:49 -0700

    feat: add Content skeleton

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,5 +1,16 @@
 import React from "react";
 
 export const Content = () => {
-
-}
+  <div>
+    <div>
+      <img />
+    </div>
+    <h4>BUILT FOR MODERN USE</h4>
+    <h3>Smarter meetings, all in one place</h3>
+    <p>
+      Send messages, share files, show your screen, and record your meetings —
+      all in one workspace. Control who can join with invite-only team access,
+      data encryption, and data export.
+    </p>
+  </div>;
+};
