1e9123f40d5e778576b7cebf5a0774dbd535818d
Michael Camele <michael.camele@gmail.com>
2021-09-19 17:59:35 -0700

    first commit w/skeleton

--- src/components/styles/StyledHeroContainer.js
+++ src/components/styles/StyledHeroContainer.js
@@ -9,5 +9,14 @@ export const StyledHeroContainer = styled.div`
     margin: 0;
     font-size: 64px;
     line-height: 64px;
+    font-weight: 900;
+    color: #28283D;
+  }
+  p {
+    margin: 0;
+    font-size: 18px;
+    line-height: 26px;
+    font-weight: 500;
+    color: #87879D;
   }
 `
