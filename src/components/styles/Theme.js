172e74cf5ddee882e3488d69308588b08f8942d1
Michael Camele <michael.camele@gmail.com>
2021-09-19 18:14:27 -0700

    feat: button theme

---
+++ src/components/styles/Theme.js
@@ -0,0 +1,10 @@
+export const Theme = {
+  primaryButton:
+    {background: "#4D96A9",
+    text: "#8FE3F9",
+    hover: "#71C0D4"},
+  secondaryButton: {
+  background: "#855FB1",
+  text: "#D9B8FF",
+  hover: "#B18BDD"}
+}
