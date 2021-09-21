d59649577da3e86ca319457bd9d982c5f78596f7
dialectrical <michael.camele@gmail.com>
2021-09-20 17:58:37 -0700

    fix: add relative units to StyledHeroContainer

--- src/components/styles/StyledHeroContainer.js
+++ src/components/styles/StyledHeroContainer.js
@@ -4,18 +4,18 @@ export const StyledHeroContainer = styled.section`
   display: flex;
   flex-direction: row;
   justify-content: space-between;
-  height: 358px;
+  height: 39.78vh;
   width: 100%;
   img {
-    height: 303px;
+    height: 33.67vh;
   }
   h2 {
-    margin: 0 47.5px 34px 47.5px;
+    margin: 0vh 3.29vw 3.77vh 3.29vw;
     font-size: 64px;
     line-height: 64px;
     font-weight: 900;
     color: #28283d;
-    width: 445px;
+    width: 30.9vw;
   }
   p {
     margin: 0;
@@ -23,7 +23,7 @@ export const StyledHeroContainer = styled.section`
     line-height: 26px;
     font-weight: 500;
     color: #87879d;
-    margin-bottom: 32px;
-    width: 535px;
+    margin-bottom: 3.56vh;
+    width: 37.15vw;
   }
 `;
