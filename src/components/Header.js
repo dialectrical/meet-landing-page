765f4080525b40e9853e2628ded62dd69883864f
dialectrical <michael.camele@gmail.com>
2021-09-20 17:50:52 -0700

    fix: add relative units to header margin

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,5 +2,5 @@ import React from "react";
 import logo from "../img/logo.svg";
 
 export const Header = () => {
-  return <img src={logo} style={{ margin: "80px 660px 108px 661px" }} />;
+  return <img src={logo} style={{ margin: "8.88vh 45.83vw 12vh 45.9vw" }} />;
 };
