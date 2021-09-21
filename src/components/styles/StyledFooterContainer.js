369c06d9e584ab7183cf63999f341faec5dd462b
dialectrical <michael.camele@gmail.com>
2021-09-20 18:54:17 -0700

    fix: add relative units to Footer

--- src/components/styles/StyledFooterContainer.js
+++ src/components/styles/StyledFooterContainer.js
@@ -3,26 +3,28 @@ import styled from "styled-components";
 export const StyledFooterContainer = styled.div`
   display: flex;
   flex-direction: row;
-  width: 1440px;
-  height: 308px;
-  margin-top: 184px;
+  width: 100vw;
+  height: 34.22vh;
+  margin-top: 20.44vh;
   img {
+    width: 100%;
+    height: 34.22vh;
     position: absolute;
     z-index: -1;
   }
   div {
     position: absolute;
-    height: 308px;
-    width: 100%;
+    height: 34.22vh;
+    width: 100vw;
     position: absolute;
     background-color: #4d96a9;
     opacity: 0.9;
     z-index: 0;
   }
   h3 {
-    width: 350px;
-    height: 88px;
-    margin: 116px 125px 104px 165px;
+    width: 34.3vw;
+    height: 9.77vh;
+    margin: 12.89vh 8.68vw 11.56vh 11.46vw;
     font-size: 40px;
     line-height: 44px;
     font-weight: 900;
@@ -30,9 +32,9 @@ export const StyledFooterContainer = styled.div`
     z-index: 1;
   }
   p {
-    width: 340px;
-    height: 78px;
-    margin: 116px 0px 114px 0px;
+    width: 23.61vw;
+    height: 8.67vh;
+    margin: 8.06vw 0 7.82vw 0;
     font-size: 18px;
     line-height: 26px;
     font-weight: 500;
@@ -40,7 +42,7 @@ export const StyledFooterContainer = styled.div`
     z-index: 1;
   }
   button {
-    margin: 131px 165px 119px 92px;
+    margin: 14.56vh 11.46vw 13.22vh 6.39vw;
     background-color: #855fb1;
     z-index: 1;
     span {
