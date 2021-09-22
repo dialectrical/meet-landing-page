df1e597aff3d4c2a2331036cd262c9b2bda1e001
Michael Camele <michael.camele@gmail.com>
2021-09-21 18:21:39 -0700

    fix: switch HeroContainerContent to relative units

--- src/components/styles/StyledHeroContainerContent.js
+++ src/components/styles/StyledHeroContainerContent.js
@@ -6,9 +6,9 @@ export const StyledHeroContainerContent = styled.section`
   margin: 0 6.11vw 0 6.11vw;
   text-align: center;
   @media (max-width: 768px) {
-    width: 457px;
-    height: 262px;
-    margin: 72px 156px 287px 155px;
+    width: 59.51vw;
+    height: 25.59vh;
+    margin: 7.03vh 20.31vw 28.03vh 20.18vw;
     flex-basis: 100%;
     order: 3;
   }
